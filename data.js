4a70102a45cdb5236372ef8724a63173f0422d0d
sleek1004 <arojojoyeo@yahoo.com>
2022-12-07 23:07:54 +0100

    music app

---
+++ data.js
@@ -0,0 +1,33 @@
+let music = [
+    {
+        name: 'song 1',
+        path:'assets/music/song1.mp3',
+        artist: 'artist 1',
+        cover: 'assets/cover/cover1.png' 
+},
+{
+    name: 'song 2',
+    path:'assets/music/song2.mp3',
+    artist: 'artist 2',
+    cover: 'assets/cover/cover2.png' 
+},
+{
+    name: 'song 3',
+    path:'assets/music/song3.mp3',
+    artist: 'artist 3',
+    cover: 'assets/cover/cover3.png' 
+},
+{
+    name: 'song 4',
+    path:'assets/music/song4.mp3',
+    artist: 'artist 4',
+    cover: 'assets/cover/cover4.png' 
+},
+{
+    name: 'song 5',
+    path:'assets/music/song5.mp3',
+    artist: 'artist 5',
+    cover: 'assets/cover/cover5.png' 
+},
+
+]
