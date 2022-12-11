102c1c90e9a5ced9087554641ec99626265ba0db
sleek1004 <arojojoyeo@yahoo.com>
2022-12-11 22:52:45 +0100

    adding eventListener

--- index.js
+++ index.js
@@ -12,3 +12,36 @@ const changeCarousel = () => {
 setInterval( ()=>{
     changeCarousel();
 },3000);
+
+const musicPlayerSection = document.querySelector('.music-player-section')
+let clickCount = 1
+
+musicPlayerSection.addEventListener('click', ()=>{
+    if(clickCount >= 2){
+        musicPlayerSection.classList.add('active');
+        clickCount = 1
+        return;
+    }
+    clickCount++;
+    setTimeout(()=>{
+        clickCount= 1;
+    }, 250);
+})
+
+const backToHomeBtn = document.querySelector('.music-player-section .back-btn')
+backToHomeBtn.addEventListener('click', ()=> {
+    musicPlayerSection.classList.remove('active')
+})
+
+const  playerlistSection = document.querySelector('.playlist');
+const navBtn = document.querySelector('.music-player-section .nav-btn')
+
+navBtn.addEventListener('click', () => {
+    playerlistSection.classList.add('active')
+    
+})
+ const backToMusicPlayer = document.querySelector('.playlist.back-btn')
+  backToMusicPlayer.addEventListener('click', () => {
+    playerlistSection.classList.remove('active')
+  })
+
